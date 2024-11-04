85c2c4832d8154eb32e2d834941d1e6af03d79c9
otontas11 <oktay1125@gmail.com>
2024-11-04 16:03:06 +0300

    ver-1

--- components/video-player/video-player.vue
+++ components/video-player/video-player.vue
@@ -1,50 +1,145 @@
 <template lang="pug">
-  div
-    .video-container
-      video(
-        ref="videoPlayer"
-        class="video-js vjs-default-skin"
-        controls
-        preload="auto"
-        width="640"
-        height="360"
-      )
+.video-player
+  .video-player-skeleton(v-if="isVisibleSkeleton" :style="[aspectRatioStyle]")
+    img.video-player-skeleton__image(:src="skeletonImage")
+
+  .video-player(v-show="video.isReadyVideoJsPlayer" :videojs-theme="video.config.theme" )
+    video.video-js.vjs-luxmty(ref="videoPlayerRef")
+     source(:src="video.src" :type="video.type" crossorigin="use-credentials")
 </template>
 
 <script>
-import { defineComponent, useContext, ref, reactive, onMounted, watch, computed } from '@nuxtjs/composition-api';
+import {defineComponent, ref, onMounted, reactive, computed} from '@nuxtjs/composition-api';
 import videojs from 'video.js';
 import 'videojs-youtube';
-
 import 'video.js/dist/video-js.css'
+import qualitySelector from 'jb-videojs-hls-quality-selector';
+import qualityLevels from 'videojs-contrib-quality-levels';
+
 
 export default defineComponent({
   name: 'NuxtTutorial',
+  props:{
+    videoJsConfig: {
+      type: Object,
+      required: false,
+      default: null,
+    },
+    src: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'video/mp4',
+    },
+  },
 
-  setup(){
-
+  setup(props, {emit}){
     // Video.js player instance and video element reference
-    const videoPlayer = ref(null)
-    let player = null
+    const videoPlayerRef = ref(null)
+     const player = ref(null);
 
-    onMounted(() => {
-      player = videojs(videoPlayer.value, {
+    const videoSrc = computed(()=> props.src );
+    const videoType = computed(() => props.type );
+    const skeletonImage = computed(() => require(`@/assets/img/preloader/skeleton.gif`));
+
+    const video=reactive({
+      isReadyVideoJsPlayer: false,
+      src: videoSrc.value,
+      type: videoType.value,
+      config:{
+        withCredentials: true,
+        aspectRatio:  props.videoJsConfig?.aspectRatio || '16:9',
         autoplay: false,
-        controls: true,
-        responsive: true,
-        fluid: true,
-        sources: [
-          {
-            src: 'https://www.youtube.com/watch?v=uODJsK6bnFc',
-            type: 'video/youtube'
+        theme:  'theme-3',
+        responsive:true,
+        preload: props.videoJsConfig?.preload || 'none',
+        playsinline: props.videoJsConfig?.playsinline || true,
+        controls: props.videoJsConfig?.controls || true,
 
-          }
-        ]
-      })
+        controlBar: {
+          volumePanel: {
+            inline: false,
+          },
+          ...props.videoJsConfig?.controlBar,
+        },
+        ...props.videoJsConfig,
+
+      }
     })
 
-    return {videoPlayer}
+    const isVisibleSkeleton = computed(() => !video.isReadyVideoJsPlayer);
+
+    const aspectRatioStyle = computed(() => {
+      const x = video.config.aspectRatio?.split(':')[0] || '16';
+      const y = video.config.aspectRatio?.split(':')[1] || '9';
+
+      return { 'aspect-ratio': `${x}/${y}` };
+    });
+
+    const isM3u8 = computed(() => {
+      if (props.src) {
+        const extension=props.src.split('.').pop()
+
+
+        if (extension === 'm3u8') {
+          return true;
+        }
+      }
+    });
+
+    onMounted(() => {
+      setupVideoJsPlayer()
+      initVideoJs()
+    })
+
+    const setupVideoJsPlayer=()=>{
+      initVideoJs();
+
+      player.value?.ready(() => {
+        video.isReadyVideoJsPlayer = true;
+      });
+
+    }
+
+    const initVideoJs=()=>{
+      player.value = videojs(videoPlayerRef.value, video.config)
+
+      if (isM3u8.value) {
+        //// kalite seçimi eklentisi
+        if (!videojs.getPlugin('qualitySelector')) {
+          videojs.registerPlugin('qualitySelector', qualitySelector);
+          player.value.hlsQualitySelector = qualitySelector;
+
+          player.value.hlsQualitySelector({
+            displayCurrentQuality: true,
+            vjsIconClass: 'vjs-icon-cog', // Ayar simgesi için
+          });
+        }
+
+        //// kalite seviyesi eklentisi
+        if (!videojs.getPlugin('qualityLevels') ) {
+          videojs.registerPlugin('qualityLevels', qualityLevels);
+        }
+
+        const qualityLevelsPlugin = player.value.qualityLevels();
+
+        qualityLevelsPlugin.on('addqualitylevel', (event) => {
+          const qualityLevel = event.qualityLevel;
+          console.log(`Available quality level: ${qualityLevel.height}p (${qualityLevel.bitrate} bps)`);
+        });
+      }
+    }
+
+    return {videoPlayerRef, video, isVisibleSkeleton, aspectRatioStyle, skeletonImage}
 
   }
 })
 </script>
+
+
+<style src="./video-player.scss" lang="scss"></style>
+
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .main-page-xx
+    b m3u8 video
+    video-player(:video-js-config="videoConfig" src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" type="application/x-mpegURL")
+    b mp4 video
+    video-player(:video-js-config="videoConfig" src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4" type="video/mp4")
+    b youtube video
+    video-player(:video-js-config="videoConfig" src="https://www.youtube.com/watch?v=dsdtjH55HCc" type="video/youtube")
+    b live
+    video-player(:video-js-config="videoConfig" src="https://nmxlive.akamaized.net/hls/live/529965/Live_1/index.m3u8" type="application/x-mpegURL")
+
+</template>
+
+<script>
+import { defineComponent, ref, reactive} from '@nuxtjs/composition-api';
+import VideoPlayer from "@/components/video-player/video-player";
+
+export default defineComponent({
+  name: 'NuxtTutorial',
+  components:{VideoPlayer},
+
+  setup(){
+    const videoConfig = reactive({
+      aspectRatio:  '16:9',
+      autoplay: true,
+
+
+    });
+///https://d1missilavut3r.cloudfront.net/v1/master/3239369ce9144762f6df79ad0250b16a882d6a39/ETV_AP_H264/index.m3u8 application/x-mpegURL
+    //https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8 application/x-mpegURL
+    //https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4
+    //video/mp4
+
+
+    ///https://www.youtube.com/watch?v=uODJsK6bnFc   video/youtube
+
+    // https://vjs.zencdn.net/v/oceans.webm  video/webm
+    return {videoConfig }
+
+}
+})
+</script>
